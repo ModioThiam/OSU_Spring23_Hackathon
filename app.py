6d040c701985dced6a5d55994fdefc273f94dc3b
ModioThiam <121213253+ModioThiam@users.noreply.github.com>
2023-05-21 23:10:22 -0400

    Update app.py
    updated app.py file

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from dotenv import load_dotenv
 load_dotenv() #take environment variables from .env
 import os
 import json
+import re
 
 
 #openai.organization = ""
@@ -18,7 +19,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///recipes.db'
 # Initialize the database
 db = SQLAlchemy(app)
 
-
 @app.route('/', methods=['POST', 'GET'])
 def homepage():
     if request.method == "GET":
@@ -28,16 +28,14 @@ def homepage():
         user_input = request.form["ingredients"]
 
         prompt = """
-        tell me a joke
-        # Give me a popular recipe represented as a JSON object that includes 3 or less similar ingredients of these
-        # ingredients: """ + user_input + """ Give me only the json object. Your output must always be in valid JSON format. I only want the object, do not talk to me.
-        # Example:
-        # {
-        #     "recipeName": <Put recipe name here>,
-        #     "ingredients": <List ingredients here>,
-        #     "instructions": <List instructions here>
-
-        # }
+        Give me a popular recipe represented as a JSON object that includes 3 or less similar ingredients of these
+        ingredients: """ + user_input + """ Give me only the json object. Your output must always be in valid JSON format ensure there is no unterminated string.
+        I only want the object, do not talk to me. Do not use the word Example in your response.
+        {
+            "recipeName": <Put recipe name here>,
+            "ingredients": <List ingredients here>,
+            "instructions": <List instructions here>
+        }
         """
 
         response = openai.Completion.create(
@@ -45,19 +43,41 @@ def homepage():
         prompt=prompt,
         temperature=0.5,
         max_tokens=1000,
-        top_p=0,
+        top_p=1,
         frequency_penalty=0
         )
 
-        response = response.choices[0].text.strip()
-        res = json.loads(response)
-        recipeName = res["recipeName"]
-        ingredients = res["ingredients"]
-        instructions = res["instructions"]    
+        def extract_recipe_info(json_str):
+            recipe_info = {}
+            recipe_dict = json.loads(json_str)
 
-        print(recipeName)
-    
-        return render_template('apitest.html', recipeName=recipeName, ingredients=ingredients, instructions=instructions)
+            # Extract recipe name
+            recipe_info["recipeName"] = recipe_dict["recipeName"]
+
+            # Extract instructions
+            instructions_str = recipe_dict["instructions"]
+            recipe_info["instructions"] = instructions_str
+
+            # Extract ingredients as a list
+            ingredients_list = recipe_dict["ingredients"]
+            recipe_info["ingredients"] = ingredients_list
+
+            return recipe_info
+
+        response_1 = response.choices[0].text.strip()
+
+        recipe_info = extract_recipe_info(response_1)
+
+        recipeName = recipe_info["recipeName"]
+        instructions = recipe_info["instructions"]
+        ingredients = recipe_info["ingredients"]
+
+        ingredientTitle = "Ingredients: "
+        instructionTitle = "Instructions: "
+
+        scroll_position = request.args.get('scroll_position', 0)
+
+        return render_template('homepage.html', recipeName=recipeName, instructions=instructions, ingredients=ingredients, ingredientTitle=ingredientTitle, instructionTitle=instructionTitle, scroll_position=scroll_position)
 
 
 
